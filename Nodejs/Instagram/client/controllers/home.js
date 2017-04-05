727e192ecb2df9091a8f26c614a3af236db0950d
hokagequan <Q@Quans-MacBook.local>
2017-04-05 20:16:58 +0800

    homepage

---
+++ Nodejs/Instagram/client/controllers/home.js
@@ -0,0 +1,17 @@
+/**
+*  Module
+*
+* Description
+*/
+angular.module('Instagram')
+.controller('HomeCtrl', function($scope){
+  
+  $scope.isAuthenticated = function () {
+    return false;
+  };
+
+  $scope.linkInstagram = function () {
+    
+  };
+
+});
