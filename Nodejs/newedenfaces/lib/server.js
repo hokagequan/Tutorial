58cbbf9436222ed2581e28c8de88dfe3968195d3
hokagequan <Q@Quans-MacBook.local>
2017-04-20 22:29:57 +0800

    目录结构更改

---
+++ Nodejs/newedenfaces/lib/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+app.set('port', process.env.PORT || '3000');
+app.use((0, _morgan2.default)('dev'));
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+app.use(_express2.default.static(_path2.default.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function () {
+	console.log('Express server listening on port ' + app.get('port'));
+});
