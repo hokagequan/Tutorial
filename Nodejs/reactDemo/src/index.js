873e8eb5014cf6026da0de6de5f1d9a6a1291fcc
hokagequan <Q@Quans-MacBook.local>
2017-04-26 23:28:35 +0800

    reactDemo

--- Nodejs/reactDemo/src/index.js
+++ Nodejs/reactDemo/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom'
 
-[
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
+// [
+//   {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+//   {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+//   {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+//   {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+//   {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+//   {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+// ];
 
 class ProductCategoryRow extends React.Component {
 	render() {
@@ -22,10 +22,58 @@ class ProductCategoryRow extends React.Component {
 
 class ProductRow extends React.Component {
 	render() {
+		let name = this.props.product.stocked ? this.props.product.name : <span style={{color: 'red'}}>{this.props.product.name}</span>;
 		return (
-			
+			<tr><td>{name}</td><td>{this.props.product.price}</td></tr>
 		);
 	}
 }
+
+class ProductTable extends React.Component {
+	render() {
+		let rows = [];
+		let lastCategory = null;
+		this.props.products.forEach( function(product) {
+			if (product.category !== lastCategory) {
+				rows.push(<ProductCategoryRow category={product.category} key={product.category} />);
+			}
+			rows.push(<ProductRow product={product} key={product.name} />);
+			lastCategory = product.category
+		});
+		return (
+			<table><thead><tr><th>Name</th><th>Price</th></tr></thead><tbody>{rows}</tbody></table>
+		);
+	}
+}
+
+class SearchBar extends React.Component {
+	render() {
+		return (
+			<form action=""><input type="text" placeholder="Search..." /><p><input type="checkbox" />{' '}Only show products in stock</p></form>
+		);
+	}
+}
+
+class FilterableProductTable extends React.Component {
+	render() {
+		return (
+			<div><SearchBar></SearchBar><ProductTable products={this.props.products}></ProductTable></div>
+		);
+	}
+}
+
+let PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
+
+ReactDom.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById('app')
+);
 
 module.hot.accept();
