18488954e60371c823f4c2e32a0ba72019092e2c
hokagequan <Q@Quans-MacBook.local>
2017-06-19 23:55:22 +0800

    project

---
+++ Nodejs/merncommentbox/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import CommentBox from './commentbox';
+
+ReactDOM.render(
+  // <App />,
+  <CommentBox />
+  document.getElementById('root')
+);
