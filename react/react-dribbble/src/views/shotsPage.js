88cba73d87f70d6219a05443b1bf2da5704bd63b
quanchengwen <quanchengwen@9rush.com>
2017-07-18 17:17:23 +0800

    shots page

--- react/react-dribbble/src/views/shotsPage.js
+++ react/react-dribbble/src/views/shotsPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/shots.css';
 
 export default class ShotsPage extends React.Component {
   constructor(props) {
@@ -8,7 +9,24 @@ export default class ShotsPage extends React.Component {
   render() {
     return (
       <div>
-      KK
+      	<nav className="navbar navbar-default">
+      		<div className="collapse navbar-collapse">
+      			<ul className="nav navbar-nav navbar-center">
+      				<li className="dropdown">
+      					<a>Popular<span className="caret"></span></a>
+      				</li>
+      				<li className="dropdown">
+      					<a>Shots<span className="caret"></span></a>
+      				</li>
+      				<li className="dropdown">
+      					<a>Now<span className="caret"></span></a>
+      				</li>
+      			</ul>
+      			<ul className="nav navbar-nav pull-right">
+      				<img alt="Brand" src="..." />
+      			</ul>
+      		</div>
+      	</nav>
       </div>
     );
   }
