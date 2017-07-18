1fcb3b31cd0ae39888d9a623532d8f237583ba6b
quanchengwen <quanchengwen@9rush.com>
2017-07-18 10:57:38 +0800

    nav bootstrap

--- react/react-dribbble/src/views/navMenuBs.js
+++ react/react-dribbble/src/views/navMenuBs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavMenuItemBs from './navMenuItemBs';
 
 export default class NavMenuBs extends React.Component {
   static propTypes = {
@@ -11,7 +12,36 @@ export default class NavMenuBs extends React.Component {
 
   render() {
     return (
-      <div></div>
+      <nav className="navbar navbar-inverse navbar-static-top">
+      	<div className="container-fluid">
+      		<div className="navbar-header">
+      			<a href="/" className="navbar-brand">Dribbble</a>
+      		</div>
+
+      		<div className="collapse navbar-collapse" id="bs-navbar-collapse">
+      			<ul className="nav navbar-nav">
+      			{
+      				this.props.menuItems.map((item) => {
+      					return (
+      						<NavMenuItemBs link={item.url} title={item.text} subItems={item.subItems}></NavMenuItemBs>
+      					);
+      				})
+      			}
+      			</ul>
+      			<form className="navbar-form pull-right">
+      				<input type="text" className="form-control" placeholder="search" />
+      			</form>
+      			<ul className="nav navbar-nav pull-right">
+      				<li>
+      					<a href="/SignUp">Sign up</a>
+      				</li>
+      				<li>
+      					<a href="/SignIn">Sign in</a>
+      				</li>
+      			</ul>
+      		</div>
+      	</div>
+      </nav>
     );
   }
 }
---
+++ react/react-dribbble/src/views/navMenuItemBs.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../css/style.css';
+
+export default class NavMenuItemBs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {isOpen: false}
+  }
+
+  render() {
+    return (
+    	<li className="dropdown">
+    		<a 
+    		  className="dropdown-toggle" 
+    		  data-toggle="dropdown" 
+    		  role="button" 
+    		  href={this.props.link} 
+    		  aria-haspopup="true" 
+    		  aria-expanded="false">
+    		  {this.props.title}
+    		</a>
+    		<ul className="dropdown-menu">
+			{
+				this.props.subItems.map((section, i) => {
+					let items =
+					section.map((item, j) => {
+						return (
+							<li><a href={item.url}>{item.text}</a></li>
+						);
+					})
+
+					if (i == this.props.subItems.length - 1) {
+						return items;
+					}
+
+					items.push(
+						<li className="divider" role="separator"></li>
+					);
+
+					return items;
+				})
+			}
+    		</ul>
+    	</li>
+    );
+  }
+}
